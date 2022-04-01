bfd56f30dcd0770f8b538789629ce35da483153f
subin sabu <subinsabu148@gmail.com>
2022-04-01 12:27:25 +0530

    deploy redirects

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import netlify from "./pages/netlify.toml";
 ReactDOM.render(<App />, document.getElementById("root"));
